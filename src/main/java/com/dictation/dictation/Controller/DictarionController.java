af2f3e544bb733888a97c7be8d7747ad7264f4db
wangbinwb <1043519367@qq.com>
2018-11-30 15:53:04 +0800

    bug

---
+++ src/main/java/com/dictation/dictation/Controller/DictarionController.java
@@ -0,0 +1,23 @@
+package com.dictation.dictation.Controller;
+import com.dictation.dictation.domain.Dictation;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+
+import javax.xml.ws.Action;
+import javax.xml.ws.RequestWrapper;
+
+/**
+ * @author wb
+ * 添加数据
+ */
+@Controller
+public class DictarionController {
+    //@Autowired
+
+   // @RequestMapping(value="/")
+    public String  addwords(Dictation dictation){
+       // Boolean b=  servlet.
+
+        return "";
+    };
+}
--- src/main/java/com/dictation/dictation/dao/dictation.java
+++
@@ -1,6 +0,0 @@
-package com.dictation.dictation.dao;
-
-public interface dictation {
-    //录入词语
-    public  int Inputtingwords();
-}
