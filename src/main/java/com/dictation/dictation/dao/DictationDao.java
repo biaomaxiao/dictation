510b30d53f652da3c5d27de0f89565c3bed94d94
zwr1124935662 <zwr15858523691@163.com>
2018-11-30 16:18:40 +0800

    完成Dao层service层

--- src/main/java/com/dictation/dictation/dao/DictationDao.java
+++ src/main/java/com/dictation/dictation/dao/DictationDao.java
@@ -1,7 +1,31 @@
 package com.dictation.dictation.dao;
 
 import com.dictation.dictation.domain.Dictation;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+
+@Repository
 public interface DictationDao {
+    /**
+     * 增加单词
+     * @param dictation
+     */
     void insertWord(Dictation dictation);
+
+    /**
+     * 删除单词
+     */
+    void  deleteWord(Dictation dictation);
+
+    /**
+     * 查询所有单词
+     */
+    List<Dictation> findAll();
+
+    /**
+     * 根据id查询单词
+     */
+    List<Dictation>  findByid(Integer id);
 }
--- src/main/java/com/dictation/dictation/service/impl/DictationServiceImpl.java
+++ src/main/java/com/dictation/dictation/service/impl/DictationServiceImpl.java
@@ -1,6 +1,35 @@
 package com.dictation.dictation.service.impl;
 
+import com.dictation.dictation.dao.DictationDao;
+import com.dictation.dictation.domain.Dictation;
 import com.dictation.dictation.service.DictationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class DictationServiceImpl implements DictationService {
+
+    @Autowired
+    private DictationDao dictationDao;
+    @Override
+    public void insertWord(Dictation dictation) {
+        dictationDao.insertWord(dictation);
+    }
+
+    @Override
+    public void deleteWord(Dictation dictation) {
+        dictationDao.deleteWord(dictation);
+    }
+
+    @Override
+    public List<Dictation> findAll() {
+        return dictationDao.findAll();
+    }
+
+    @Override
+    public List<Dictation> findByid(Integer id) {
+        return dictationDao.findByid(id);
+    }
 }
